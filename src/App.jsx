8068f1f6f761cbfbe8d6fda8e191028e2ba63297
Brain-Lady <brain.lady404@gmail.com>
2025-05-21 19:40:32 +0300

    done

--- src/App.jsx
+++ src/App.jsx
@@ -7,17 +7,29 @@ import ContactForm from './components/ContactForm/ContactForm';
 import { useState } from 'react';
 import { useDebounce } from 'use-debounce';
 import { useMemo } from 'react';
+import { useEffect } from 'react';
 
 function App() {
   const icons = [<IoPerson />, <FaPhoneAlt />];
   const [inputValue, setInputValue] = useState("");
   const [debouncedInputValue] = useDebounce(inputValue, 300);
-  const [contacts, setContacts] = useState([
-    { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
-    { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
-    { id: "id-3", name: "Eden Clements", number: "645-17-79" },
-    { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
-  ]);
+  const [contacts, setContacts] = useState(() => {
+    const savedObject = window.localStorage.getItem("saved-contacts");
+    if (savedObject !== null) {
+      return JSON.parse(savedObject);
+    }
+    return [
+      { id: "id-1", name: "Rosie Simpson", number: "459-12-56" },
+      { id: "id-2", name: "Hermione Kline", number: "443-89-12" },
+      { id: "id-3", name: "Eden Clements", number: "645-17-79" },
+      { id: "id-4", name: "Annie Copeland", number: "227-91-26" },
+    ]
+  });
+  
+  useEffect(() => {
+    window.localStorage.setItem("saved-contacts", JSON.stringify(contacts))
+  }, [contacts]);
+
 
 
   const visibleContacts = useMemo(() => {
